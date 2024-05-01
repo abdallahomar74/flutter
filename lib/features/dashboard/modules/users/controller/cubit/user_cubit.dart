c8c1b75c07691339245e5c4f964fb3251e6ac9ed
zyadkroush <zyadkroush>
2024-05-01 17:03:39 +0300

    zyad_connection ui to data base

--- lib/features/dashboard/modules/users/controller/cubit/user_cubit.dart
+++ lib/features/dashboard/modules/users/controller/cubit/user_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:section_project/features/dashboard/modules/users/model/repo/local_db(sqflite).dart';
+import 'package:section_project/features/dashboard/modules/users/model/user_model.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  UserCubit() : super(UserLoading()){
+    init();
+  }
+
+   List<UModel> users = [];
+  Future<void> init() async {
+    emit(UserLoading());
+     users = await (await DatabaseRepo.instance).fetch_data();
+    if (users.isEmpty) {
+      emit(UserEmpty());
+    } else {
+      emit(UserLoaded());
+    }
+  }
 }
--- lib/features/dashboard/modules/users/view/component/user_item.dart
+++ lib/features/dashboard/modules/users/view/component/user_item.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:section_project/features/dashboard/modules/users/model/user_model.dart';
 
 class UserItem extends StatelessWidget {
-  const UserItem({super.key,});
-  
+  const UserItem({super.key, required this.uModel,});
+  final UModel uModel;
   @override
   Widget build(BuildContext context) {
-    return const ColoredBox(
+    return  ColoredBox(
       color: Colors.green,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'XXX',
-            style: TextStyle(
+            uModel.name??'*****',
+            style: const TextStyle(
               fontSize: 20,
               color: Colors.black,
             ),
           ),
-          Padding(
+           Padding(
             padding: EdgeInsets.only(left: 10),
             child: Text(
-               'XXX',
+               uModel.address??'*****',
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.yellow,
